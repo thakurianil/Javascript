058c9582c34468849aae03740bd43223f0355a23
Anil khand <anilkhandthakuri@gmail.com>
2024-08-14 13:25:12 +1000

    Update

--- script.js
+++ script.js
@@ -9,3 +9,49 @@ truthStatement = true;
 
 console.log(truthStatement);
 
+var object = {
+    name: "anil",
+    phone: 10000,
+    isMember: true
+}
+
+console.log(object.name);
+
+Array_number =[ 100, 200, 300];
+
+console.log(typeof Array_number)
+console.log(Array_number [0]);
+
+ sum =( a, b) =>{
+    return a +b;
+}
+
+console.log(sum (1,2));
+
+today = new Date();
+
+console.log(today);
+
+console.log(typeof today);
+
+console.log(typeof Array_number);
+
+console.log(typeof sum);
+
+console.log(Math.PI);
+
+
+
+const grade = 49;
+
+if (grade >= 80){
+    console.log("A grade")
+} else if(grade >= 60){
+    console.log("B grade")
+} else if(grade >= 40){
+    console.log("C grade")
+}else{
+    console.log("F grade")
+}
+
+
