7ec29161dc285037d485456875767556a67c0518
Anil khand <anilkhandthakuri@gmail.com>
2024-08-14 13:54:30 +1000

    Update

--- script.js
+++ script.js
@@ -54,4 +54,44 @@ if (grade >= 80){
     console.log("F grade")
 }
 
+var ternary = 9;
+var temporaryVariable = 0;
 
+temporaryVariable =  ternary == 10 ? ternary : ternary ==9? 90 : 100;
+
+console.log(temporaryVariable);
+
+
+a = 9;
+b = 5;
+
+y = a < 10 ? a * 4 : b==5 ? b * 3 : 0; 
+
+console.log(y);
+
+let variable = 10;
+
+
+try{
+    let variable = 10;
+
+}
+catch (error){
+    console.log(error());
+}
+
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+ 
+i =0 ;
+while (i<10) {
+    console.log(i);
+    i++;
+}
+
+i=8;
+do {
+    console.log("Do while")
+    i++;
+} while (i<10);
